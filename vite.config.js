1005c5cd733d72f0eae5070174459c2b5575d619
Talha-HATSS <130793244+talhapredator@users.noreply.github.com>
2025-05-08 17:57:01 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/upload': 'https://2b5b-202-47-34-45.ngrok-free.app', // Your FastAPI port
-      '/query': 'https://2b5b-202-47-34-45.ngrok-free.app'
+      '/upload': 'https://f365-202-47-34-45.ngrok-free.app', // Your FastAPI port
+      '/query': 'https://f365-202-47-34-45.ngrok-free.app'
     }
   },
   plugins: [react()],
